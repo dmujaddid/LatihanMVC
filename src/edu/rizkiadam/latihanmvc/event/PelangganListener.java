2f89b1757bb3b84edc3f3d0da793c82293a08927
Roby <43459626+dmujaddid@users.noreply.github.com>
2018-12-27 10:52:26 +0700

    Add files via upload

---
+++ src/edu/rizkiadam/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkiadam.latihanmvc.event;
+
+import edu.rizkiadam.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * NIM      : 10116599
+ * Nama     : Robby Eka
+ * Kelas    : PBO6K
+ */
+public interface PelangganListener {
+
+    public void onChange(PelangganModel pelanggan);
+
+}
---
+++ src/edu/rizkiadam/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkiadam.latihanmvc.model;
+
+/**
+ *
+ * NIM      : 10116599
+ * Nama     : Robby Eka
+ * Kelas    : PBO6K
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+
+}
